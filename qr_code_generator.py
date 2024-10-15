0f6fe24e5c432fc606320d90e1f6fafb520fec50
Madusanka-Bandara <kasunmadhusanka44@gmail.com>
2024-10-15 20:32:52 +0530

    Initial setup for version 2

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,9 +1,6 @@
+import tkinter as tk
+from tkinter import filedialog, colorchooser, messagebox
 import qrcode
 from pyzbar.pyzbar import decode
 from PIL import Image
 
-myqrcode = qrcode.make("Hello!")
-myqrcode.save("myqrcode.png", scale=8)
-
-decode_msg = decode(Image.open("myqrcode.png"))
-print(decode_msg[0].data.decode("ascii"))
