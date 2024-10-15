002d588ef148099bbfa7b913b08f27c371471f63
Madusanka-Bandara <kasunmadhusanka44@gmail.com>
2024-10-15 20:48:31 +0530

    Added main application window

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,6 +1,18 @@
+import os
 import tkinter as tk
 from tkinter import filedialog, colorchooser, messagebox
 import qrcode
 from pyzbar.pyzbar import decode
 from PIL import Image
 
+# Ensure assets folder exits for save the qr codes
+if not os.path.exists('assets'):
+    os.makedirs('assets')
+
+# Main application window
+root = tk.Tk()
+root.title("QR Code Generator")
+root.geometry("400x500")
+
+# Run the application
+root.mainloop()
